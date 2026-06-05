94cfa5b3c065db45fa5cf2d98039d7deaa388af3
bellandry <bellandry.work@gmail.com>
2026-06-05 09:51:30 +0100

    feat: create invoices table & save invices in queue job

---
+++ migrations/1780648293731_invoices-table.js
@@ -0,0 +1,29 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.createTable("invoices", {
+    id: "serial primary key",
+    userId: "integer not null",
+    eventId: "integer not nul",
+    quantity: "integer not null",
+    status: "varchar(20) default 'generated'",
+    createdAt: "timestamp default now()",
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropTable("invoices");
+};
--- worker.js
+++ worker.js
@@ -1,5 +1,6 @@
 const { Worker } = require("bullmq");
 const connection = require("./redis");
+const pool = require("./db");
 
 // Heavy PDF generator simulator
 const heavyPdfGenerator = (duration = 500) => {
@@ -16,24 +17,35 @@ const waitNetwork = (duration = 50) => {
 const worker = new Worker(
   "ticket-processing",
   async (job) => {
+    const client = await pool.connect();
     console.log(
       `[WORKER]: Treatment Started for job #${job.id} for user ${job.data.email}`,
     );
 
     try {
+      const { userId, eventId, quantity } = job.data;
+
       //Heavy job
       heavyPdfGenerator();
+
+      const invoice = await client.query(
+        "INSERT INTO invoices (userId, eventId, quantity) VALUES($1, $2, $3) RETURNING *",
+        [userId, eventId, quantity],
+      );
+      const newInvoice = invoice.rows[0];
 
       //do some heavy work
       await waitNetwork();
 
       console.log(
-        `[WORKER]: Treatment Completed for job #${job.id}, pdf generated & email sent`,
+        `[WORKER]: Treatment Completed for job #${job.id}, pdf generated & email sent. invoice N° : ${newInvoice.id}`,
       );
     } catch (error) {
       console.error(
         `[WORKER]: Treatment Failed for job #${job.id}, err : ${error}`,
       );
+    } finally {
+      client.release();
     }
   },
   { connection, concurrency: 2 },
